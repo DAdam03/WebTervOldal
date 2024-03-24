09c5aab5f8fd5d7330321e3365c4aa14c9aab3ca
DAdam03 <97408089+DAdam03@users.noreply.github.com>
2024-03-24 14:10:15 +0100

    Delete profile-menu.js

--- profile-menu.js
+++
@@ -1,8 +0,0 @@
-function openMenu() {
-    let profileBtn = document.getElementById("profileMenu");
-    if (profileBtn.style.display != "block") {
-        profileBtn.style.display = "block";
-    } else {
-        profileBtn.style.display = "none";
-    }
-}
